a2982a6a7e0fa06a4bc164b270d472c22f5d3650
miiv0 <minzana2002@gmail.com>
2025-03-29 22:17:32 -0400

    add blank sentence, for every repeat add another word to end of sentence.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+    let sentence = ""
+    for (let i = 0; i < num; i++) {
+        sentence += word
+    }
+    return sentence
 };
 
 // Do not edit below this line
