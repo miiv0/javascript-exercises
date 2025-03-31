9245246db8be3a567e07194e7978533eafc59d34
miiv0 <minzana2002@gmail.com>
2025-03-31 16:36:57 -0400

    uesd equations to convert F to C and rounded the decimal to the 1st decimal point.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let convertToC = (temp - 32) * 5 / 9;
+  let rounded = Math.round(convertToC * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let convertToF = (temp * (9 / 5)) + 32;
+  let rounded = Math.round(convertToF * 10) / 10;
+  return rounded;
+
 };
 
 // Do not edit below this line
