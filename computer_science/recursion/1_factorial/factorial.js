f3c4bddc7b2ba44885138994bea952008f8b85c1
miiv0 <minzana2002@gmail.com>
2026-06-24 18:13:21 -0400

    start solution

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,16 @@
-const factorial = function() {
-
+const factorial = function factorial(n) {
+    if (n <= 0) {
+        console.log("Done")
+        return
+    }
+    console.log(n)
+    const n = n * n
+    console.log(n)
+    factorial(n)
 };
+
+console.log(factorial(3))
+
 
 // Do not edit below this line
 module.exports = factorial;
